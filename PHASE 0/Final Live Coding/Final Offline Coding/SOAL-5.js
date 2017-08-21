875ffbd01d88fa9fb2c1746f19940944b5e8492a
Harynp <harynugrahaputra@gmail.com>
2017-08-21 10:27:47 +0700

    BENERIN FOLDER

--- PHASE 0/Final Live Coding/Final Offline Coding/SOAL-5.js
+++ PHASE 0/Final Live Coding/Final Offline Coding/SOAL-5.js
@@ -15,34 +15,51 @@ Output : c(1) , 2(2) ,b(3) ,9(4)
 */
 
 function mostFrequentItem(arr){
-var final = [];
-var temp = [];
+
+	var result = [];
+	var unik = [];
+	var jadi = [];
+
 	for(i=0;i<arr.length;i++){
-		if(temp.indexOf(arr[i]) === -1){
-			temp.push(arr[i])
+
+		if(unik.indexOf(arr[i]) === -1){
+			unik.push(arr[i]);
 		}
 	}
-	var hasil =  temp.sort().reverse();
-	
-	for(j=0;j<hasil.length;j++){
-	var result = [];
+	var balik = unik.sort();
+
+	var jumlah = [];
+	for(j=0;j<balik.length;j++){
+		var count = 0;
 		for(k=0;k<arr.length;k++){
-			if(hasil[j]===arr[k]){
-				result.push(arr[k]);
+			if(balik[j]===arr[k]){
+				count++;
 			}
 		}
-		final.push(result.length);
-
+		jumlah.push(count);
 	}
-if(final.length<4){
-	return hasil[0]+'('+final[0]+'), '+hasil[1]+'('+final[1]+'), '+hasil[2]+'('+final[2]+')';
-}else if(final.length=4){
-	return hasil[0]+'('+final[0]+'), '+hasil[3]+'('+final[3]+'), '+hasil[1]+'('+final[1]+'), '+hasil[2]+'('+final[2]+')';
-}
 
-return final;
 
+
+		// if(balik.length < 4){
+		// result.push(balik[2]+'('+jumlah[2]+') '+balik[1]+'('+jumlah[1]+') '+balik[0]+'('+jumlah[0]+')');
+		// } else if(balik.length === 4){
+		// result.push(balik[3]+'('+jumlah[3]+') '+balik[0]+'('+jumlah[0]+') '+balik[2]+'('+jumlah[2]+') '+balik[1]+'('+jumlah[1]+')');
+		// }
+	for (l=0; l < jumlah.length;l++){
+		jadi.push('('+jumlah[l]+')'+balik[l]);
+	}
+	jadi.sort();
+	
+	for (m = 0; m<jadi.length;m++) {
+		result.push(jadi[m].slice(3)+jadi[m].slice(0,3));
+	}
+	return result
+	
 }
+// 		result.push(balik[l]+ '('+jumlah[l]+')');
+// 	} 	
+	// var sementara = result.reverse();
 
 console.log(mostFrequentItem(['asus', 'asus', 'samsung', 'iphone','iphone', 'asus','asus'])); // samsung(1), iphone(2) , asus(4)
 console.log(mostFrequentItem([9,'b','b','c',9,9,'b',9, 2,2])); // c(1) , 2(2) ,b(3) ,9(4)
--- PHASE 0/Final Live Coding/Final Offline Coding/koreksi4.js
+++ PHASE 0/Final Live Coding/Final Offline Coding/koreksi4.js
@@ -8,7 +8,6 @@ Name : ___
 Disediakan sebuah function numberGrouping yang bertugas untuk menerima sebuah parameter berupa angka, 
 dan mengembalikan susunan angka baru yang merupakan hasil pengelompokan berdasarkan 3 digit angka. 
 Apabila jumlah angka yang belum dikelompokan ada 4 maka akan dibagi menjadi 2 buah 2 digit angka tidak 3 dan 1
-
 Ex: Input: [ 1 , 2 , 3 , 4 , 5 , 6 ]
 Output: 123-456
 Input: [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 1]
@@ -19,21 +18,32 @@ Output: 123-456-78
 */
 
 function numberGrouping(angka) {
-return angka.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "-");
+  var len = angka.length;
+  var tampung = []
+  for (var i=0; i<len; i++) {
+    if (i%3 === 0 && i !== 0) {
+      tampung.push('-');
+      tampung.push(angka[i]);
+    } else {
+      tampung.push(angka[i]);
+    }
+  }
+  console.log(tampung)
+
+  var len1 = tampung.length;
+  if (tampung[len1-2] === '-') {
+    var a = tampung[len1-2];
+    var b = tampung[len1-3];
+    tampung[len1-2] = b;
+    tampung[len1-3] = a;
+    console.log(a);
+    console.log(b);
+    console.log(tampung);
+  }
+  return tampung.join('');
 }
-
-
-// return angka.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-// var temp = [];
-// for(i=0;i<angka.length;i++){
-	
-// 	if(angka[i].length%3==0){
-// 		temp.push('-');
-// 	} else{
-// 		temp.push(angka[i]);
-// }
-
 
 console.log(numberGrouping([4,6,7,9,2,9])); //467-929
 console.log(numberGrouping([6,3,1,8,4,7,9,3,5,8])); //631-847-93-58
 console.log(numberGrouping([1,8,4,7,3,7,1])); //184-73-71
+console.log(numberGrouping([1,8,4,7,3,7,1,4])); //184-737-14
